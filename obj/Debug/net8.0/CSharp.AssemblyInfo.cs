c9468be3a5887c1dc7cde8b6d4e74bd5fdbc5a80
vinicius <vininiquini7@gmail.com>
2024-05-01 12:30:07 -0300

    Finalizei a configuração do ambiente

--- obj/Debug/net8.0/CSharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/CSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20c41899d98d770ed9379ef5cfe11fa767f1a52c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f20bd10cf5e41fc42c4beeb56d623536a93c8c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
